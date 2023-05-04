1e4bdb5035f108ac14883ff3ac21104ae4e149a7
Sh13ldVusal <sh13ldbtc@gmail.com>
2023-05-04 14:29:48 -0700

    name

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-lee-5os@c1b6t7c$eck8s6v9ze!*vps!-pg-nuy&yiu!#js*ys
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["6e83-68-235-44-24.ngrok-free.app"]
+ALLOWED_HOSTS = ["asanodeme.net"]
 
 
 # Application definition
