c9be23b1f0a0b82f1b71eb23336d621f1dfe037d
Sh13ldVusal <sh13ldbtc@gmail.com>
2023-05-05 16:10:36 -0700

    test

--- config/settings.py
+++ config/settings.py
@@ -52,9 +52,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'asanpay.middleware.VisitCountMiddleware',
     'asanpay.middleware.BanIPMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     ]
 
 ROOT_URLCONF = 'config.urls'
