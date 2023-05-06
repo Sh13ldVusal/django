6f3df09958d38eb334bce232b18d3051863c5724
Sh13ldVusal <sh13ldbtc@gmail.com>
2023-05-06 09:54:32 -0700

    asdas

--- config/urls.py
+++ config/urls.py
@@ -9,7 +9,7 @@ from django.conf.urls import handler404
 urlpatterns = [
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-    path('admin/', admin.site.urls),
+    path('adminasdasda/', admin.site.urls),
     path('', include('asanpay.urls')),
 ]
 
